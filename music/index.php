4f7fc97ed6d26af1023a01984fc2d8d7af9641d3
Lonax <deniswaren77@gmail.com>
2020-06-15 10:38:49 +0200

    Initial Release

---
+++ music/index.php
@@ -0,0 +1,56 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<body scroll="no" style="overflow: hidden">
+	<div class="divide"></div>
+	<head>
+	<meta name="keywords" content ="Dont touch it !">
+	<meta name="author" content ="Vector"
+	<meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	   <link rel="icon" href="Favicon.ico">
+     <link type="text/css" rel="stylesheet" href="https://lonax.cf/files/materialize.css" media="screen,projection">
+    <link rel="stylesheet" href="https://lonax.cf/files/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="https://lonax.cf/files/style.css">
+	<script type="text/javascript" src="https://lonax.cf/files/jquery.js"></script>
+	<link rel="stylesheet" type="text/css" href="https://lonax.cf/files/star-animation.css">
+	
+
+	
+	
+    <title>Welcome</title>
+    <script>
+			///// magic title changer big wooshies /////
+			(function titleScroller(text) {
+			document.title = text;
+			setTimeout(function() {
+			titleScroller(text.substr(1) + text.substr(0, 1,));
+			}, 500);
+			}(" Lonax "));
+	</script>
+
+<!--
+autoplay muted id="bgvidhh"
+-->
+	<video autoplay muted id="bgvidhh">
+  <source src="./files/vid.mp4" type="video/mp4">
+</video>
+<embed src="./files/vid.mp4?track=1" volume="0.001" autostart="true" width="0" height="0">
+
+<div id="stars"></div>
+<div id="stars1"></div>
+<div id="stars2"></div>
+
+</head>
+
+	<style type="text/css">* {cursor: url(https://lonax.cf/files/cur817.cur), auto !important;}</style> <div class="contain">
+
+	<h1 class="font-effect-anaglyph"><a href="#">Lonax</href></h1>
+<iframe src="https://open.spotify.com/embed/user/spotify/playlist/37i9dQZEVXcVH3rqgWMvgL" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+	<a class="waves-effect waves-light btn grey darken-4" a="" target="" href="https://lonax.cf/">Go Back</a><a>
+<br><br><br><br>
+
+<hr>
+</a></div><a>
+
+
+<script src="./files/disabledev.js"></script>
+</a></body><div style="top: -200.453px; left: 913px;"></div></html>
